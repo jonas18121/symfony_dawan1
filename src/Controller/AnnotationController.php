020b5144f9888d62a83fad68d755c9adea89c5fe
Jonas971 <jonasxtrm971@gmail.com>
2020-03-24 10:13:32 +0100

    Annotation et twig

---
+++ src/Controller/AnnotationController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/annotation", methods="GET")
+ */
+class AnnotationController extends  AbstractController
+{
+
+    /**
+    * @Route("")
+     */
+    public function index()
+    {
+        return $this->render('annotation/index.html.twig',[
+            'annotation' => 'Annotation'
+        ]);
+    }
+
+}
